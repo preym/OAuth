0cdcb8640798d2e787e6433621702dfc1aaebd13
The Egghead Creative <ehc@kaveri.(none)>
2014-04-21 13:09:46 +0530

    Integrated linkedin from socialauth-android

---
+++ src/main/java/com/ehc/OAuth/LinkedinActivity.java
@@ -0,0 +1,17 @@
+package com.ehc.OAuth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ehc
+ * Date: 21/4/14
+ * Time: 11:16 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LinkedinActivity extends Activity {
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
